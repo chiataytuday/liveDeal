e14ab22afa3586c08880bd7b39fcd9b9e634fb41
claudio <barbera.claudio@gmail.com>
2012-11-11 11:27:54 +0100

    Initial Commit

---
+++ liveDeal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  liveDeal
+//
+//  Created by claudio barbera on 11/11/12.
+//  Copyright (c) 2012 claudio barbera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
